f6db74289ebdc8668f94d0d7ecbb885ac47e3b20
kai-ilki <kailam11223@gmail.com>
2019-11-06 23:53:36 +0800

    model

--- backtrack_app/projectManage/models.py
+++ backtrack_app/projectManage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+
+class Deverloper(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    project = models.ForeignKey(Project,on_delete=models.SET_NULL, blank=True, null=True)
+
+
