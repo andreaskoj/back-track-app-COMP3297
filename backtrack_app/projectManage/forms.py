59a40fd1290f31903b3f0617499d48b56fb21678
Andreas Koj <kojandreas@gmail.com>
2019-11-19 16:21:27 +0800

    fix -> prevent adding PO to a project as a developer

--- backtrack_app/projectManage/forms.py
+++ backtrack_app/projectManage/forms.py
@@ -8,9 +8,9 @@ class CreateProjectForm(forms.Form):
         user = kwargs.pop('user')
         super(CreateProjectForm, self).__init__(*args, **kwargs)
         self.fields['developer'].choices = [(dev.user.username, dev.user.username) for dev in
-                                            Developer.objects.exclude(user__username=user).filter(project=None)]
+                                            Developer.objects.exclude(user__username=user).exclude(
+                                                isProductOwner=True).filter(project=None)]
 
     project_name = forms.CharField(max_length=100)
     manager = forms.ChoiceField(choices=[(manager, manager) for manager in ScrumMaster.objects.all()])
     developer = forms.ChoiceField()
-
